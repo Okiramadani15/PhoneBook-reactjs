2111dcbefaab2b6f5f648eeea86831e7676eb1b7
okiramadani15 <okiramadani53@gmail.com>
2023-11-21 14:02:39 +0700

    new update

---
+++ src/components/TodoBox.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import TodoForm from "./TodoForm"
+import TodoList from "./TodoList"
+
+export default function TodoBox(){
+    const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    setTodos([
+      {title},
+      ...todos
+    ])
+}
+  return (
+  <>
+    <TodoForm add={addTodo} />
+    <TodoList todos= {todos} />
+  </>
+  )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import TodoItem from "./TodoItem";
+
+
+export default function TodoList({todos = [] }){
+    const todosNode = todos.map((item, index) => (<TodoItem todo={item}/>))
+
+    return (
+        <ul>
+            {todosNode}
+        </ul>
+    )
+
+
+}
